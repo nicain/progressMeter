1fa50602ada00881d7979ff7fa1f4378fca8c910
Nicholas Cain <nicain.seattle@gmail.com>
2010-05-05 17:58:21 -0700

    Time remaining

--- example.py
+++ example.py
@@ -5,3 +5,15 @@
 #  Created by nicain on 5/5/10.
 #  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
 #
+
+import progressMeter as pm
+import time
+import random
+
+total = 20
+p = pm.ProgressMeter(total=total)
+
+while total > 0:
+    p.update(1)
+    time.sleep(1)
+    total -= 1
